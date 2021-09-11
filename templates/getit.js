70b67d2d50f5ef644c746f7b8b648d44fc288ab3
bernardo Cunha Capoferri <bernardocc@al.insper.edu.br>
2021-09-11 13:22:35 -0300

    tudo funcionando e tudo que foi requisitado funcionando

--- templates/getit.js
+++ templates/getit.js
@@ -3,7 +3,18 @@ function getRandomInt(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+var noteCard = "note-"
+var noteModifyCard = "note-modify-"
 
+function openUpdateForm(id) {
+  document.getElementById(noteCard.concat(String(id))).style.display = "none"
+  document.getElementById(noteModifyCard.concat(String(id))).style.display = "flex"
+}
+
+function closeUpdateForm(id) {
+  document.getElementById(noteCard.concat(String(id))).style.display = "flex"
+  document.getElementById(noteModifyCard.concat(String(id))).style.display = "none"
+}
 document.addEventListener("DOMContentLoaded", function () {
   // Faz textarea aumentar a altura automaticamente
   // Fonte: https://www.geeksforgeeks.org/how-to-create-auto-resize-textarea-using-javascript-jquery/#:~:text=It%20can%20be%20achieved%20by,height%20of%20an%20element%20automatically.
@@ -27,6 +38,4 @@ document.addEventListener("DOMContentLoaded", function () {
       5
     )} card-rotation-${getRandomInt(1, 11)}`;
   }
-
-  
 });
